f18e81653029e660746c7e258c1766288bba7fc7
Lara Capila <laracapilak@gmail.com>
2021-08-19 18:56:43 -0300

    Setting context

---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import ClimaContext from './ClimaContext';
+
+function ClimaProvider({ children }) {
+  const [cityName, setCityName] = useState({});
+  const context = {
+    cityName,
+    setCityName,
+  };
+
+  return (
+    <ClimaContext.Provider value={ context }>
+      { children }
+    </ClimaContext.Provider>
+  );
+}
+
+ClimaProvider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+  ]).isRequired,
+};
+
+export default ClimaProvider;
